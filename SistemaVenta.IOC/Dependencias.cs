480e01eb5e567ada82f4964482f0e69dd69ba710
uzielduarte <duarteuziel36@gmail.com>
2022-11-19 16:06:54 -0600

    Add project files.

---
+++ SistemaVenta.IOC/Dependencias.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencias
+    {
+        public static void InyectarDependencia(this IServiceCollection Service, IConfiguration Configuration)
+        {
+            Service.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
